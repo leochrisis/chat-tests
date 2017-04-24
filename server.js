321012ce3605a1056ba7d5f7114d8710a06b761d
leochrisis <leonardo.chfc@gmail.com>
2017-04-23 21:52:17 -0300

    Building project's tools

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var clients = {};
+
+http.listen(3000, function(){
+console.log('listening on port 3000');
+});
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.use(express.static('js'))
+
+io.on("connection", function (client) {
+  client.on("join", function(name){
+    console.log("Joined: " + name);
+    clients[client.id] = name;
+    client.emit("update", "You have connected to the server.");
+    client.broadcast.emit("update", name + " has joined the server.")
+  });
+
+  client.on("send", function(msg){
+    console.log("Message: " + msg);
+    client.broadcast.emit("chat", clients[client.id], msg);
+  });
+
+  client.on("disconnect", function(){
+    console.log("Disconnect");
+    io.emit("update", clients[client.id] + " has left the server.");
+    delete clients[client.id];
+  });
+});
